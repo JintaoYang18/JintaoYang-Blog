6168696f6a4e7f2e266c5730f9ae9f213f8af6c0
JintaoYang18 <90304586+JintaoYang18@users.noreply.github.com>
2022-03-09 22:12:52 +0800

    123

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,50 @@
+module.exports = {
+    // 站点配置
+    base: '/JintaoYang-Blog/',
+    lang: 'zh-CN',
+    title: 'Yang\'s Blog',
+    description: '日有寸进',
+    head: [['link', { rel: 'icon', href: './images/logo2.png' }]],
+    
+  
+    // 主题和它的配置
+    theme: '@vuepress/theme-default',
+    themeConfig: {
+        navbar: [
+            // 嵌套 Group - 最大深度为 2
+            {
+              text: '读论文笔记',
+              children: [
+                {
+                    text: '攻击1',
+                    children: ['/group/sub/foo.md', '/group/sub/bar.md'],
+                },
+                {
+                    text: '防御2',
+                    children: ['/group/sub/foo.md', '/group/sub/bar.md'],
+                  },
+              ],
+            },
+            // 控制元素何时被激活
+            {
+              text: 'Vuepress学习',
+              children: [
+                {
+                  text: 'Always active',
+                  link: '/',
+                  // 该元素将一直处于激活状态
+                  activeMatch: '/',
+                },
+                {
+                  text: 'Active on /foo/',
+                  link: '/not-foo/',
+                  // 该元素在当前路由路径是 /foo/ 开头时激活
+                  // 支持正则表达式
+                  activeMatch: '^/foo/',
+                },
+              ],
+            },
+        ],
+        logo: '/images/logo2.png',
+    },
+  }
---
+++ docs/README.md
@@ -0,0 +1,28 @@
+---
+lang: zh-CN
+home: true
+title: 主页
+heroImage: /images/logo2.png
+actions:
+  - text: 开始
+    link: /guide/getting-started.html
+    type: primary
+  - text: 介绍
+    link: /guide/
+    type: secondary
+features:
+  - title: 更高
+    details: Spring
+  - title: 更快
+    details: Summer
+  - title: 更强
+    details: Autumn
+  - title: 更团结
+    details: Winter
+    
+footer: MIT Licensed | Copyright © 2022-present Jintao Yang
+---
+
+# Hello VuePress
+
+VuePress 2 已经发布 :tada: ！
